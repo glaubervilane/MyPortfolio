46a724ecb1df69d64db551133bfe6e6b96a1f7ad
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-11-15 17:15:51 -0700

    Launch the first implementation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My Portfolio</h2>
+    </div>
+  );
+}
+
+export default App;
